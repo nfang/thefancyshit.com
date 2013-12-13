328dacc18b2ff208a66c387e03a23ecfc0e111eb
Nathan Fang <nyyrikkifang@gmail.com>
2013-12-13 23:32:46 +1100

    Extend slide.js; consolidate animation code

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,28 +6,16 @@ require.config({
 });
 
 require([ "jquery", "slider" ], function ($) {
-
-  function rotateKV() {
-    var kv = $("#kv")
-      , numOfItems = $("#kv > li").length
-      , startOffset = parseFloat(kv.css('left'))
-      , endOffset = 0;
-
-    if ((startOffset - 960) > -numOfItems * 960) {
-      endOffset = startOffset - 960;
-    }
-
-    kv.animate({
-      left: endOffset
-    }, 1000);
-  }
-
   $(function () {
     $("#carousel").slider({
-        prevTrigger : '.slide-nav.prev',
-        nextTrigger : '.slide-nav.next'
-      });
+      prevTrigger : '.slide-nav.prev',
+      nextTrigger : '.slide-nav.next'
+    });
 
-    setInterval(function () { rotateKV() }, 8000);
+    $("#kv").slider({
+      autoPlay : true,
+      duration : 1000,
+      interval : 8000
+    });
   });
 });
--- public/scripts/slider.js
+++ public/scripts/slider.js
@@ -3,27 +3,59 @@ define([ "jquery" ], function ($) {
   "use strict";
 
   var Slider = function (el, options) {
+    var defaults = {
+      duration : 300,
+      interval : 5000,
+      autoPlay : false
+    };
+
     this.$el = $(el);
     this.items = this.$el.children('.item');
     this.items.eq(0).addClass('active');
 
-    if (options && options.prevTrigger) {
-      $(options.prevTrigger).on('click', $.proxy(this.prev, this));
+    this.options = $.extend(defaults, options || {});
+
+    if (this.options.prevTrigger) {
+      $(this.options.prevTrigger).on('click', $.proxy(this.prev, this));
     }
 
-    if (options && options.nextTrigger) {
-      $(options.nextTrigger).on('click', $.proxy(this.next, this));
+    if (this.options.nextTrigger) {
+      $(this.options.nextTrigger).on('click', $.proxy(this.next, this));
+    }
+
+    if (this.options.autoPlay) {
+      setInterval($.proxy(this.rotate, this), this.options.interval);
     }
   }
 
   Slider.prototype = {
+    rotate: function () {
+      var activeItem = this.items.filter('.active')
+        
+        , nextItem = activeItem.next().length ? 
+            activeItem.next() : this.items.eq(0)
+        
+        , start = parseFloat(this.$el.css('left'))
+
+        , end = activeItem.index() < this.items.length - 1 ? 
+            start - (activeItem.width() + nextItem.width()) / 2 :
+            0;
+
+      this.$el.animate({ 'left': end }, this.options.duration);
+      this.items.removeClass('active');
+      nextItem.addClass('active');
+    },
+
     prev: function () {
       var activeItem = this.items.filter('.active')
         , prevItem = activeItem.prev()
         , offset = parseFloat(this.$el.css('left'));
 
       if (activeItem.index() > 0) {
-        this.$el.animate({ 'left': offset + (activeItem.width() + prevItem.width()) / 2 }, 300);
+        this.$el.animate({ 
+          'left': offset + (activeItem.width() + prevItem.width()) / 2
+        }, this.options.duration);
+
         this.items.removeClass('active');
         prevItem.addClass('active');
       }
@@ -35,7 +67,10 @@ define([ "jquery" ], function ($) {
         , offset = parseFloat(this.$el.css('left'));
 
       if (activeItem.index() < this.items.length - 1) {
-        this.$el.animate({ 'left': offset - (activeItem.width() + nextItem.width()) / 2 }, 300);
+        this.$el.animate({ 
+          'left': offset - (activeItem.width() + nextItem.width()) / 2
+        }, this.options.duration);
+
         this.items.removeClass('active');
         nextItem.addClass('active');
       }
