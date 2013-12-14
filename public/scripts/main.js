8452bb07526d6d0867937925429a9d568ea4665f
Nathan Fang <nyyrikkifang@gmail.com>
2013-12-14 16:03:56 +1100

    Update jquery version; add requirejs path fallbacks

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,10 @@
 require.config({
   "baseUrl": "/scripts",
   "paths"  : {
-    "jquery": "//code.jquery.com/jquery-1.10.2.min"
+    "jquery" : [
+      "//code.jquery.com/jquery-1.10.2.min",
+      "vendor/jquery.min"
+    ]
   }
 });
 
